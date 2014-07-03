c3a87d3468aaca6f6c7d2b7b628fbbcc77e93ba5
vovanz <vovanzagorodsky@gmail.com>
2014-07-03 18:34:29 +0600

    helpers

---
+++ helpers.js
@@ -0,0 +1,10 @@
+function builtinRead(x) {
+    if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
+        throw "File not found: '" + x + "'";
+    return Sk.builtinFiles["files"][x];
+}
+
+shuffle = function(v){
+    for(var j, x, i = v.length; i; j = parseInt(Math.random() * i), x = v[--i], v[i] = v[j], v[j] = x);
+    return v;
+};
